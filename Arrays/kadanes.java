fac8c8355476049cb62b5ca32b0f1028a1140f39
unknown <vanshjain0311@gmail.com>
2023-10-10 08:46:12 +0530

    updated kadanes algorithm

--- Arrays/kadanes.java
+++ Arrays/kadanes.java
@@ -2,25 +2,25 @@ package Vansh.Arrays;
 // all the edge cases are covered
 public class kadanes {
     public static int maxSUM(int arr[],int n){
-        int largest=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-         if(arr[i]>largest){
-           largest = arr[i];
-          }
-         } 
+    //    int largest=Integer.MIN_VALUE;
+    //     for(int i=0;i<n;i++){
+    //      if(arr[i]>largest){
+    //        largest = arr[i];
+    //       } 
+    //      } 
          
-         if(largest<0){
-             return largest;
-         }
+        //  if(largest<0){
+        //      return largest;
+        //  }
             int sum=0;
             int maxSum=0;
              for(int i=0; i<n; i++) {
                  sum = sum + arr[i];
-                 if(sum<0) {
-                     sum=0;
-                 }
                  if(maxSum<sum) {
                      maxSum=sum;
+                 }
+                 if(sum<0) {
+                     sum=0;
                  }
            }
              return maxSum;
--- Arrays/max_subarraySum_Kadanes.java
+++ Arrays/max_subarraySum_Kadanes.java
@@ -4,21 +4,21 @@ public class max_subarraySum_Kadanes {
     public static void printMaxsum(int numbers[]){
         int currSum=0;
         int maxSum=Integer.MIN_VALUE;
-        
         for(int i=0;i<numbers.length;i++){
             currSum = currSum + numbers[i];
-            if(currSum<0){
-                currSum=0;
-            }
+            
                if(maxSum<currSum){
                maxSum=currSum;
              }
+             if(currSum<0){
+                currSum=0;
+            }
          }   
        System.out.println("our max sub array sum is = " + maxSum);
     }
 
     public static void main(String[] args) {
-        int numbers[]={-2, 1, -3, 4, -1, 2 ,1, -5, 4};
+        int numbers[]={-2, -3, -1, -5, -4};
        printMaxsum(numbers);
 }
 }
