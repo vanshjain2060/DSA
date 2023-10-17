e81d6e26b9ba504e434f7349e8e23ee44eb1bd7c
unknown <vanshjain0311@gmail.com>
2023-10-17 08:55:25 +0530

    Update Search

--- Two_D_Arrays/stairCaseSearch.java
+++ Two_D_Arrays/stairCaseSearch.java
@@ -1,18 +1,19 @@
 package Vansh.Two_D_Arrays;
 
+import java.util.Arrays;
+
 public class stairCaseSearch {
             
 // This method can be used to solve "SORTED" 2D arrays
 // jo array assending ya decending me sort nhi hoga usme yen method nhi lagega
 
-    public static boolean stairSearch(int matrix[][] , int key) {
+    public static int[] stairSearch(int matrix[][] , int key) {
         int row =0 , col =matrix[0].length-1 ;
      
         while(row < matrix.length && col >= 0){
 
             if(matrix[row][col] == key){
-                System.out.println( "forund the key at (" + row + "," + col + ")");
-                   return true;
+                return new int[]{row,col};
             }
             else if(key < matrix[row][col]){
                 col-- ;
@@ -22,7 +23,7 @@ public class stairCaseSearch {
             }
         } 
         System.out.println("key not found");
-        return false;
+        return new int[]{-1,-1};
     }
     public static void main(String args[]) {
         int matrix[][] = {{10, 20, 30, 40},
@@ -30,7 +31,9 @@ public class stairCaseSearch {
                           {27, 29, 37, 48},
                           {32, 33, 39, 50}};
        int key = 27;
-       stairSearch(matrix, key);
+      System.out.println(Arrays.toString(stairSearch(matrix, key)));
+      // to print the value of array Arrays.toString method is used which 
+      //takes the input as array and print the array int the form oof list
     } 
 }
 
