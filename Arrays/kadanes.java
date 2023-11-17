61a1e7980660572628c6c1458cd7a03b9a19d151
unknown <vanshjain0311@gmail.com>
2023-11-18 00:21:12 +0530

    kadanes

--- Arrays/kadanes.java
+++ Arrays/kadanes.java
@@ -13,7 +13,7 @@ public class kadanes {
         //      return largest;
         //  }
             int sum=0;
-            int maxSum=0;
+            int maxSum=Integer.MIN_VALUE;
              for(int i=0; i<n; i++) {
                  sum = sum + arr[i];
                  if(maxSum<sum) {
