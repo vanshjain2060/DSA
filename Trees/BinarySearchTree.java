42af200d66f565c9c6c6a53cf89f751ccee32c14
unknown <vanshjain0311@gmail.com>
2024-03-16 02:15:20 +0530

    BST

--- Trees/BinarySearchTree.java
+++ Trees/BinarySearchTree.java
@@ -70,17 +70,76 @@ public class BinarySearchTree {
         return Math.abs( (height(node.left) - height(node.right)) )  <= 1  &&  balanced(node.left) && balanced(node.right);
     }
 
-
+// this will takes the input as array and insert elements of array into the tree using insert function 
     public void populate(int nums[]) {
         for(int i=0; i<nums.length; i++) {
             insert(nums[i]);
         }
     }
 
+
+    // populate function for the sorted array as input
+    public void populateSorted(int nums[]) {
+        populateSorted(nums, 0 , nums.length);
+    }
+    private void populateSorted(int[] nums, int start, int end) {
+        if(start >= end) { return; }
+
+        int mid = (start + end) / 2;
+        // insert middle element and then call for left and right function call
+        insert(nums[mid]);
+
+        populateSorted(nums, start, mid);
+        populateSorted(nums, mid+1, end);
+    }
+
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
-        int nums[] = {5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
-        tree.populate(nums);
-        tree.display();
+        int nums[] = {1, 2, 3, 4, 5 ,6, 7 ,8, 9};
+        tree.populateSorted(nums);
+        tree.preOrder();
+        System.out.println();
+        tree.InOrder();
+        System.out.println();
+        tree.postOrder();
     }
-} 
+ 
+
+// TRAVERSALS
+
+    public void  preOrder() {
+        preOrder(root);
+    }
+    private void preOrder(Node node) {
+        if(node == null) return;
+
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    
+    
+    public void  InOrder() {
+        InOrder(root);
+    }
+    private void InOrder(Node node) {
+        if(node == null) return;
+
+        InOrder(node.left);
+        System.out.print(node.value + " ");
+        InOrder(node.right);
+    }
+
+
+    public void  postOrder() {
+        postOrder(root);
+    }
+    private void postOrder(Node node) {
+        if(node == null) return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " ");
+    }
+
+}
