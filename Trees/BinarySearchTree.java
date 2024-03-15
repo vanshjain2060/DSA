b9312ce11dca94c5db098f5a581f5528031a308d
unknown <vanshjain0311@gmail.com>
2024-03-15 23:35:32 +0530

    tree

--- Trees/BinarySearchTree.java
+++ Trees/BinarySearchTree.java
@@ -2,4 +2,74 @@ package Vansh.Trees;
 
 public class BinarySearchTree {
     
-}
+    public class Node {
+        private int value , height;
+        private Node left , right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getvalue() {
+            return value;
+        }
+    }
+
+    private Node root;
+
+    public BinarySearchTree() {
+
+    }
+
+    public int height(Node node) {
+        if(node == null) return -1;
+        return node.height;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    
+    public void display() {
+        display(root, "Root Node: ");
+    }
+    private void display(Node node, String details) {
+        if(node==null) return;
+        System.out.println(details + node.getvalue());
+
+        display(node.left, "Left child of " + node.getvalue() + " : ");
+        display(node.right, "right child of " + node.getvalue() + " : ");
+    }
+
+
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+    private Node insert(int value, Node node) {
+        if(node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if(value < node.value) {
+            node.left = insert(value, node.left);
+        }
+        if(value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left) , height(node.right)) + 1;
+
+        return node;
+    }
+
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+    private boolean balanced(Node node) {
+        if(node == null) return true;
+        
+        return Math.abs( (height(node.left) - height(node.right)) )  <= 1  &&  balanced(node.left) && balanced(node.right);
+    }
+} 
