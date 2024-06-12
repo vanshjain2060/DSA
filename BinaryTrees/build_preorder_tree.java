cda8ba3ab12b42797448ca998aa781b9ff4acff2
vanshjain2060 <vanshjain0311@gmail.com>
2024-06-13 00:23:48 +0530

    diameter of tree approach 1

--- BinaryTrees/build_preorder_tree.java
+++ BinaryTrees/build_preorder_tree.java
@@ -70,36 +70,52 @@ public class build_preorder_tree {
             }
         }
     
-        public static int height(Node root) {
+        public int height(Node root) {
             if(root == null) return 0;
             int lh = height(root.left);
             int rh = height(root.right);
             return Math.max(lh, rh) + 1;
         }
 
-        public static int countNode(Node root) {
+        public int countNode(Node root) {
             if(root == null) return 0;
             int leftCount = countNode(root.left);
             int rightCount = countNode(root.right);
             return leftCount + rightCount + 1;
         }
     
-        public static int sum(Node root) {
+        public int sum(Node root) {
             if(root == null) return 0;
             int leftSum = sum(root.left);
             int rightSum = sum(root.right);
             return (leftSum + rightSum + root.data);
         }
+    
+        public int diameter(Node root) { // O(n^2)
+            if(root == null) return 0;
+
+            int leftDia = diameter(root.left);
+            int rightDia = diameter(root.right);
+            int leftHei = height(root.left);
+            int rightHei = height(root.right);
+
+            int selfDia = leftHei + rightHei + 1;
+
+            return Math.max(selfDia , Math.max(leftDia, rightDia));
+        }
+    
+        
     }
 
 
     public static void main(String[] args) {
-        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, 8, -1, -1};
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         tree.levelOrder(root);
         System.out.println("Number of nodes present in the tree is: " + tree.countNode(root));
         System.out.println("Height of the root Node of the tree is: " + tree.height(root));
         System.out.println("Total sum of all the Nodes of the tree is: " + tree.sum(root));
+        System.out.println("Diameter of the tree is: " + tree.diameter(root));
     }
 }
