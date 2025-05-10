4bff6db760d86db6f620e263c6b455b63373bcb7
vanshjain2060 <vanshjain0311@gmail.com>
2025-05-10 14:28:47 +0530

    postfix to infix

---
+++ Stacks/PostfixToInfix.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class PostfixToInfix {
+
+    static String postfixToInfix(String exp) {
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+
+            if (Character.isLetterOrDigit(c)) { 
+                stack.push(String.valueOf(c));
+            } else {
+                String operand2 = stack.pop();
+                String operand1 = stack.pop();
+
+                // Form the infix expression by wrapping it in parentheses
+                String infix = "(" + operand1 + c + operand2 + ")";
+                stack.push(infix);
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String exp = "pq+mn-*";
+        System.out.println("Postfix expression: " + exp);
+        System.out.println("Infix expression: " + postfixToInfix(exp));
+    }
+}
