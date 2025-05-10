a9a5d16c34ea18cf244be9b739bf2e01d012a61f
vanshjain2060 <vanshjain0311@gmail.com>
2025-05-10 15:08:25 +0530

    postfix to prefix

---
+++ Stacks/PostfixToPrefix.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class PostfixToPrefix {
+    public static String postfixToPrefix(String exp) {
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+
+            if (Character.isLetterOrDigit(c)) {
+                stack.push(String.valueOf(c));
+            } else {
+                String operand2 = stack.pop();
+                String operand1 = stack.pop();
+
+                // Form the prefix expression by placing the operator before the operands
+                String prefix = c + operand1 + operand2;
+                stack.push(prefix);
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String exp = "pq+mn-*";
+        System.out.println("Postfix expression: " + exp);
+        System.out.println("Prefix expression: " + postfixToPrefix(exp));
+    }
+}
